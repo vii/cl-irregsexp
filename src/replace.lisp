5ec854774ce8ecc9018cb771f55c58a564021c93
John Fremlin <john@fremlin.org>
2009-12-20 05:28:56 +0000

    do not allocate a buffer for replacements unless at least one replacement is done

--- src/replace.lisp
+++ src/replace.lisp
@@ -29,49 +29,48 @@
 	    result
 	    (floor (length result) 2))))))
 
-(defmacro match-replace-helper (string &key op after result match-replacements)
-  (with-unique-names (start results-len add)
-    `(let ((,start 0) (,results-len 0))
-       (declare (type integer-match-index ,start)
-		(type fixnum ,results-len)
-	       (dynamic-extent ,start ,results-len))
+(defmacro match-replace-helper (string &key op after result-init match-replacements)
+  (with-unique-names (start results-len add result replace-block)
+    `(block ,replace-block
+       (let ((,start 0) (,results-len 0) ,result)
+	 (declare (type integer-match-index ,start)
+		  (type fixnum ,results-len)
+		  (dynamic-extent ,start ,results-len ,result))
 	 (flet ((,add (x) 
 		  (let ((len (length x))) 
 		    (unless (zerop len) 
 		      (incf ,results-len len) 
+		      (unless ,result (setf ,result ,result-init))
 		      (vector-push-extend x ,result)))))
 	   (declare (inline ,add) (dynamic-extent #',add))
-       (match-bind 
-	  (,op '(setf ,start pos)
-	       t
-	       (or ,@(loop for (match replacement) in match-replacements
-			   collect 
-			   `(progn
-			      ,match
-			      '(,add (target-subseq ,start (match-until-start)))
-			      '(,add (force-to-target-sequence ,replacement))))
-		     (progn (last) '(,add (target-subseq ,start pos))))
-	       ,@(when after `('(,add ,after))))
-	  ,string)
-       (replace-concatenate-sequences
-	,string
-	,results-len
-	,result)))))
+	   (match-bind 
+	       (,op '(setf ,start pos)
+		    t
+		    (or ,@(loop for (match replacement) in match-replacements
+				collect 
+				`(progn
+				   ,match
+				   '(,add (target-subseq ,start (match-until-start)))
+				   '(,add (force-to-target-sequence ,replacement))))
+			(progn (last) 
+			       '(if ,result
+				 (,add (target-subseq ,start pos))
+				 (return-from ,replace-block ,string))))
+		    ,@(when after `('(,add ,after))))
+	       ,string)
+	   (replace-concatenate-sequences
+	    ,string
+	    ,results-len
+	    ,result))))))
 	
 
 (defmacro match-replace-one (string &body match-replacements)
   "As match-replace-all but at most one replacement is made"
-  (with-unique-names (result)
-    (once-only (string)
-      `(let ((,result (make-array 3 :fill-pointer 0 :adjustable nil)) )
-	 (declare (dynamic-extent ,result))
-	 (match-replace-helper ,string :result ,result :op progn :after (match-remaining) :match-replacements ,match-replacements)))))
+  (once-only (string)
+    `(match-replace-helper ,string :result-init (make-array 3 :fill-pointer 0 :adjustable nil) :op progn :after (match-remaining) :match-replacements ,match-replacements)))
 
 (defmacro match-replace-all (string &body match-replacements)
   "For each (match replacment) in MATCH-REPLACEMENTS replace each value of match with the value of replacement in STRING"
-  (with-unique-names (result)
-    (once-only (string)
-      `(let ((,result (make-array (length ,string) :fill-pointer 0 :adjustable t)))
-	 (declare (dynamic-extent ,result))
-	 (match-replace-helper ,string :result ,result :op * :match-replacements ,match-replacements)))))
+  (once-only (string)
+    `(match-replace-helper ,string :result-init (make-array (length ,string) :fill-pointer 0 :adjustable t) :op * :match-replacements ,match-replacements)))
 
